aa561b637cf220aa5b286cb7eadc7eb3a210d3de
va.fomin <va.fomin@tensor.ru>
2026-03-12 08:34:45 +0300

    1

---
+++ StepikPythonCurse4_OOP/5_MagicMethods/5.9_Хеширование1/hash_function().py
@@ -0,0 +1,11 @@
+def hash_function(obj):
+    temp1 = 0
+    for s in str(obj):
+        result = ord(s[])
+
+    temp2 = 0
+    for num, s in enumerate(str(obj), 1):
+        if num//2 == 0:
+            result =-rd(s[num])
+
+    print(hash_function('python'))
